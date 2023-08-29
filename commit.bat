760e5656d12c824015ecf55ab2a2d7359a293e20
Bryan Ortiz <bryansom1207@hotmail.com>
2023-08-29 09:39:07 -0500

    Ultimo commit

--- commit.bat
+++ commit.bat
@@ -1,9 +1,6 @@
 git add .
 git commit -m "Ultimo commit"
 git push
-set GOOS=linux
-set GOARCH=amd64
-set CGO_ENABLED=0
-GOARCH=amd64 GOOS=linux go build main.go -ldflags="-s -w"
+go build main.go
 del main.zip
 tar.exe -a -cf main.zip main.exe
